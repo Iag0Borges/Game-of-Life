eaa0382694aaeebaffb884f65c38ddd52c15e02f
Iag0Borges <iagojogoonline@gmail.com>
2024-05-04 23:43:37 -0300

    Created Game of Life

---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <raylib.h>
+#include <iostream>
+#include "simulation.hpp"
+
+int main()
+{
+	Color GREY = { 29, 29, 29, 255 };
+	const int WINDOW_WIDTH = 1200;
+	const int WINDOW_HEIGHT = 800;
+	const int CELL_SIZE = 4;
+	int FPS = 12;
+
+	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Game of Life");
+	SetTargetFPS(FPS);
+	Simulation simulation{ WINDOW_WIDTH, WINDOW_HEIGHT, CELL_SIZE };
+	
+	while (WindowShouldClose() == false)
+	{
+		if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+		{
+			Vector2 mousePosition = GetMousePosition();
+			int row = mousePosition.y / CELL_SIZE;
+			int column = mousePosition.x / CELL_SIZE;
+			simulation.ToggleCell(row, column);
+		}
+		if (IsKeyPressed(KEY_ENTER))
+		{
+			simulation.Start();
+			SetWindowTitle("Game of Life is running ...");
+		}
+		else if (IsKeyPressed(KEY_SPACE))
+		{
+			simulation.Stop();
+			SetWindowTitle("Game of Life has stopped.");
+		}
+		else if (IsKeyPressed(KEY_F))
+		{
+			FPS += 2;
+			SetTargetFPS(FPS);
+		}
+		else if (IsKeyPressed(KEY_S))
+		{
+			if (FPS > 5)
+			{
+				FPS -= 2;
+				SetTargetFPS(FPS);
+			}
+		}
+		else if (IsKeyPressed(KEY_R))
+		{
+			simulation.CreateRamdomState();
+		}
+		else if (IsKeyPressed(KEY_C))
+		{
+			simulation.ClearGrid();
+		}
+
+		simulation.Update();
+
+		BeginDrawing();
+		ClearBackground(GREY);
+		simulation.Draw();
+		EndDrawing();
+	}
+
+	CloseWindow();
+}
